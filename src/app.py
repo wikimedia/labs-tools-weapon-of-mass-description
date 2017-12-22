cad9577c4afd1ac72eda431ef89b9ffd4f45874e
Martin Urbanec <martin.urbanec@wikimedia.cz>
2017-12-22 21:05:11 +0100

    Hello world-like version, all basic fces are working

---
+++ src/app.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import flask
+import os
+import yaml
+import simplejson as json
+import requests
+from urllib.parse import quote
+from flask import redirect, request, jsonify, make_response, render_template
+import mwoauth
+import mwparserfromhell
+from requests_oauthlib import OAuth1
+import random
+import toolforge
+import smtplib
+from email.mime.text import MIMEText
+
+app = flask.Flask(__name__)
+application = app
+
+ua = "Weapon of Mass Description (https://tools.wmflabs.org/weapon-of-mass-description; martin.urbanec@wikimedia.cz"
+requests.utils.default_user_agent = lambda: ua
+
+__dir__ = os.path.dirname(__file__)
+app.config.update(yaml.safe_load(open(os.path.join(__dir__, 'config.yaml'))))
+
+key = app.config['CONSUMER_KEY']
+secret = app.config['CONSUMER_SECRET']
+
+def logged():
+	return flask.session.get('username') != None
+
+def getusername():
+	return flask.session.get('username')
+
+@app.after_request
+def after_request(response):
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+	response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+	return response
+
+@app.route('/')
+def index():
+	username = flask.session.get('username')
+	if username is not None:
+		return render_template('tool.html', logged=logged(),username=getusername())
+	else:
+		return render_template('login.html', loggeed=logged(), username=getusername())
+
+@app.route('/login')
+def login():
+	"""Initiate an OAuth login.
+	Call the MediaWiki server to get request secrets and then redirect the
+	user to the MediaWiki server to sign the request.
+	"""
+	consumer_token = mwoauth.ConsumerToken(
+		app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+	try:
+		redirect, request_token = mwoauth.initiate(
+		app.config['OAUTH_MWURI'], consumer_token)
+	except Exception:
+		app.logger.exception('mwoauth.initiate failed')
+		return flask.redirect(flask.url_for('index'))
+	else:
+		flask.session['request_token'] = dict(zip(
+		request_token._fields, request_token))
+		return flask.redirect(redirect)
+
+
+@app.route('/oauth-callback')
+def oauth_callback():
+	"""OAuth handshake callback."""
+	if 'request_token' not in flask.session:
+		flask.flash(u'OAuth callback failed. Are cookies disabled?')
+		return flask.redirect(flask.url_for('index'))
+	consumer_token = mwoauth.ConsumerToken(app.config['CONSUMER_KEY'], app.config['CONSUMER_SECRET'])
+
+	try:
+		access_token = mwoauth.complete(
+		app.config['OAUTH_MWURI'],
+		consumer_token,
+		mwoauth.RequestToken(**flask.session['request_token']),
+		flask.request.query_string)
+		identity = mwoauth.identify(app.config['OAUTH_MWURI'], consumer_token, access_token)
+	except Exception:
+		app.logger.exception('OAuth authentication failed')
+	else:
+		flask.session['request_token_secret'] = dict(zip(access_token._fields, access_token))['secret']
+		flask.session['request_token_key'] = dict(zip(access_token._fields, access_token))['key']
+		flask.session['username'] = identity['username']
+
+	return flask.redirect(flask.url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+	"""Log the user out by clearing their session."""
+	flask.session.clear()
+	return flask.redirect(flask.url_for('index'))
+
+if __name__ == "__main__":
+	app.run(debug=True, threaded=True)
