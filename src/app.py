18e0b9972c4747a207cc312a56e02d2d6a5ebd9c
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-01-03 14:27:45 +0100

    How did it get there?

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-							<li><a href="https://phabricator.wikimedia.org/project/board/3165/">Bugs</a></li>
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
